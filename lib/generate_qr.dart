96317db3b4a40cb9dcd5927cbfa40f437b4e3725
Dhairya <allroundre.d7@gmail.com>
2025-04-03 16:47:11 +0530

    2nd commit

--- lib/generate_qr.dart
+++ lib/generate_qr.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+//add qr_flutter dependency in pubspec/yaml
+//then import below package
+import 'package:qr_flutter/qr_flutter.dart';
+
+//stateful widget
 class GenerateQr extends StatefulWidget {
   const GenerateQr({super.key});
 
@@ -8,25 +13,43 @@ class GenerateQr extends StatefulWidget {
 }
 
 class _GenerateQrState extends State<GenerateQr> {
+
+  //used for extracting user input text from
   TextEditingController textCtr=TextEditingController();
+  //storing textField text into qrData
+  String qrData="";
+
   @override
   Widget build(BuildContext context) {
 
+    //textField decoration
     InputDecoration decoration = InputDecoration(
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(27)),
         labelText: "Enter Text",
         hintText: "https://www.google.com",
         suffixIcon: IconButton(
-            onPressed: (){}, icon: Icon(Icons.check_outlined))
+            onPressed: (){
+              //setState for update changing input text
+              setState(() {
+                qrData=textCtr.text.trim();
+              });
+            }, icon: Icon(Icons.check_outlined))
     );
 
-
+    //widget for user input
     TextField enteredText = TextField(controller: textCtr,decoration:decoration,);
 
+    //widget for qr image generation
+    QrImageView qrImg= QrImageView(
+      data: qrData,
+      version: QrVersions.auto,
+      size: 200,
+      errorCorrectionLevel: QrErrorCorrectLevel.H,);
 
+    //widget for show child widget in column format
+    Column column=Column(mainAxisAlignment:MainAxisAlignment.center,children: [qrImg,enteredText],);
 
-    Column column=Column(mainAxisAlignment:MainAxisAlignment.center,children: [enteredText],);
-
+    //returning center widget which contains all child widgets
     return Center(
         child: ConstrainedBox(constraints: BoxConstraints(maxWidth: 500),
             child: Padding(padding: EdgeInsets.all(30),child: column,)));
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'generate_qr.dart';
 import 'scan_qr.dart';
 
+//main function
 void main() {
   runApp(const MyApp());
 }
 
+//stateful widget
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -17,19 +19,27 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
 
+    //tabs- boxes
     Tab gen_qr_tab = Tab(text: "Generate QR",icon: Icon(Icons.qr_code),);
     Tab scan_qr_tab = Tab(text: "Scan QR",icon: Icon(Icons.qr_code_scanner),);
 
+    //tab bar- add tabs in tab bar
     TabBar tabBar= TabBar(tabs: [scan_qr_tab,gen_qr_tab],indicatorColor: Colors.purpleAccent,);
 
+    //tab bar view- which tab screen will show when click on tab
+    //list function according to tab sequence
     TabBarView tabBarView= TabBarView(children: [ScanQr(),GenerateQr()]);
 
+    //appbar- top bar
     AppBar appBar=AppBar(title:Text("QR Scan & Generate"),backgroundColor: Colors.purple);
 
+    //scaffold
     Scaffold scaffold = Scaffold(appBar: appBar,body: tabBarView,bottomNavigationBar: tabBar,);
 
+    //default tab when app is starting
     DefaultTabController defaultTab=DefaultTabController(length: 2, child: scaffold);
 
+    //returning material app
     return MaterialApp(home: defaultTab,debugShowCheckedModeBanner: false,);
   }
 }
